0633300aa978afb03b40752402bc2f1905bbdb6d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-11 14:32:15 +0530

    type annotation works

--- types/type_annotation.js
+++ types/type_annotation.js
@@ -1,15 +1,10 @@
 //? Introduction To Types in Typescript
-
 //* In Typescript, Type Annotation is a way of explicitly specifying the type of a variable, function parameter, or function return value.
-
 //! Advantages📌
 //? It helps the typescript compiler to enforce type checking and provide type safety during development.
 //? What operations could we performed on that variable or value.
-
 //? Type annotations are expressed by using a colon(:) followed by the desired type after the variable or function parameter declaration. There can be space after the colon.
-
-let num1: number = 10;
-let str1: string = "Hello";
-
+var num1 = 10;
+var str1 = "Hello";
 //* num1 = "kirtti";
 console.log(num1.toString());
