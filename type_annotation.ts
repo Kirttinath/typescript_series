db34138b0ef81c42c4ad677d8f34e39d46ad2672
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-11 13:04:51 +0530

    what is Type annotation and its advantages

---
+++ type_annotation.ts
@@ -0,0 +1,9 @@
+//? Introduction To Types in Typescript
+
+//* In Typescript, Type Annotation is a way of explicitly specifying the type of a variable, function parameter, or function return value.
+
+//! Advantages📌
+
+//? It helps the typescript compiler to enforce type checking and provide type safety during development.
+
+//?
