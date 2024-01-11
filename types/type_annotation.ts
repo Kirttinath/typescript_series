f9b3e541f7eea5317d4c0fa8b5b909f0c29eaadd
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-11 14:25:40 +0530

    type annotation added

--- types/type_annotation.ts
+++ types/type_annotation.ts
@@ -2,6 +2,9 @@
 
 //* In Typescript, Type Annotation is a way of explicitly specifying the type of a variable, function parameter, or function return value.
 
+let num1: number = 10;
+let str1: string = "Hello";
+
 //! Advantages📌
 //? It helps the typescript compiler to enforce type checking and provide type safety during development.
 //? What operations could we performed on that variable or value.
