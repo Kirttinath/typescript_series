95cd1d843fefad69c621687f0dceaf03d0e65192
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-11 12:10:36 +0530

    add function

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-console.log("I am Kirttinath");
-var num = 1;
-/*num = "vinod";  Type 'string' is not assignable to type 'number'*/
-num = 65;
+/*1st Program */
+/*console.log("I am Kirttinath");
+
+let num: number = 1;*/
+/*num = "vinod";  Type 'string' is not assignable to type 'number'
+num = 65;*/
+/*2nd program (add two numbers) */
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(5, 7));
+console.log(sum(7, "Number"));
--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
-console.log("I am Kirttinath");
+//*1st Program
+//*console.log("I am Kirttinath");
 
-let num: number = 1;
-/*num = "vinod";  Type 'string' is not assignable to type 'number'*/
-num = 65;
+//*let num: number = 1;
+//*num = "vinod";  Type 'string' is not assignable to type 'number'
+//*num = 65;
+
+//*2nd program (add two numbers)
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(add(7, 3)); //*O/P :7Number because in parameter we don't use type annotation
