62afbf3043a28f9368b61cd03408d0d1f10bb764
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-11 18:20:05 +0530

    a function checks even or not

--- types/boolean_types.js
+++ types/boolean_types.js
@@ -1,14 +1,12 @@
 //* Boolean Types :
-
 //? In Typescript, the boolean type represents a value that can be either true or false. It is one of the basic primitive typess in the language.
-
-let ismyName: boolean = true;
+var ismyName = true;
 // ismyName = "yes"; Invalid
-
 //! Check Even Number
-
-function isEven(evnum: number): boolean {
-  if (evnum % 2 == 0) return true;
-  else return false;
+function isEven(evnum) {
+    if (evnum % 2 == 0)
+        return true;
+    else
+        return false;
 }
 console.log(isEven(2));
