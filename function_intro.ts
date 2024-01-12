bfba8deb5930f63da1fc2412a7b89f41e6d0446c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-12 12:53:27 +0530

    function declaration definition

---
+++ function_intro.ts
@@ -0,0 +1,3 @@
+//* Function Declaration :
+// ? We can declare a function using the function keyword, followed by the function name, a list of parameters enclosed in paranthesis, and a return type.
+// ? The function body contains the code that will be executed when the function will called.
