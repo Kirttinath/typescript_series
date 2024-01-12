ee1e4b30a7d24a90eaf39f7f2fb5d408cf709771
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-12 11:55:35 +0530

    anytype in typescript example

--- types/any_unknown.ts
+++ types/any_unknown.ts
@@ -4,8 +4,11 @@
 
 //? You will see any type when you first write the code.
 
+let favnum: any = 34;
+favnum = "Kirtti";
+
 //! useCases :
 
 //* Workingg with Dynamic Data : When dealing with data from dynamic sources like user inputs, networks responses, or deserialized JSON objects, rhe any type can be useful.
 
-//*Migration from Javascript : When migrating an existing javascript to Typescript, using the any type can be a convinient way to quick
+//*Migration from Javascript : When migrating an existing javascript codebase to Typescript, using the any type can be a convinient way to quickly annote variables and functions without immidiately specifying their precise types.
