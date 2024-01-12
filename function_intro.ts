a99197bc5376edf6e186c850e43f9a09b6938350
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-12 13:01:25 +0530

    function invocation definition added

--- function_intro.ts
+++ function_intro.ts
@@ -1,3 +1,7 @@
 //* Function Declaration :
 // ? We can declare a function using the function keyword, followed by the function name, a list of parameters enclosed in paranthesis, and a return type.
 // ? The function body contains the code that will be executed when the function will called.
+
+//* Function Invocation :
+// ? To execute a function, you simply invoke it by using its name followed by parantheses.
+// ? We can provide arguments (actual values) for the parameters defined in the function declaration.
