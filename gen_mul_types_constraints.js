752b0764bc44a97c0dd0910bcb381aba6650ca78
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-15 01:10:27 +0530

    generic multiple types constraints explanation with examples and explanation

---
+++ gen_mul_types_constraints.js
@@ -0,0 +1,13 @@
+//* IMP: when we use generic and non generic type together then while calling the function we need to defined the type in a manner but with non generics we don't
+// function add(a: number, b:number): number:
+// function add(a: string, b: string): string;
+// function add(a: any, b: any): any {
+//return ab;
+//}
+function add(a, b, c) {
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+var results1 = add(2, "Kirtti", true);
+var results2 = add("Student", 24, false);
